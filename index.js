a8c3227bb02d145917c2ee65fb360dd92c0d288a
juanCardozoGmail <juanguillermopc@gmail.com>
2024-07-24 13:49:39 -0500

    fix on /routes/index about /api/v#

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routerApi = require('./routes/index.js');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
-const apiVersion = '/api/v1';
+const espress = require('express');
 
 function routerApi(app) {
-  app.use(apiVersion + '/products', productsRouter);
-  app.use(apiVersion + '/users', usersRouter);
-  app.use(apiVersion + '/categories', categoriesRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  app.use('/products', productsRouter);
+  app.use('/users', usersRouter);
+  app.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
