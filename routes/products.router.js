96a55050770895ce2a5037045e25415789130520
juanCardozoGmail <juanguillermopc@gmail.com>
2024-07-26 11:07:38 -0500

    async await productService

--- routes/products.router.js
+++ routes/products.router.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ProductServices = require('./../services/products.service');
 const service = new ProductServices();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -12,30 +12,29 @@ router.get('/filter', (req, res) => {
   res.send('ruta products/filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const id = req.params.id;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const product = service.create(body);
+  const product = await service.create(body);
   res.json(product);
   //res.json({ message: 'created', data: body });
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  const product = service.update(id, body);
+  const product = await service.update(id, body);
   res.json(product);
-  //  res.json({ message: 'update', data: body, id });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const proDel = service.delete(id);
+  const proDel = await service.delete(id);
   res.json(proDel);
 });
 
--- services/products.service.js
+++ services/products.service.js
@@ -18,7 +18,7 @@ class ProductServices {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.string.uuid(),
       ...data,
@@ -28,15 +28,19 @@ class ProductServices {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((f) => f.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -49,7 +53,7 @@ class ProductServices {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found for deleting');
